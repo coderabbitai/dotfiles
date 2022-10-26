4cc66f7fd17bb9a60474f0d4f6fc1e7b2c7fb126
Harjot Gill <harjot.gill@fluxninja.com>
2022-10-26 18:32:09 -0500

    remove old migration code in sync_fluxninja.sh

--- sw/bin/executable_sync_fluxninja.sh
+++ sw/bin/executable_sync_fluxninja.sh
@@ -1,48 +1,11 @@
 #!/usr/bin/env bash
 
-# if ~/Work/fluxninja/RM exists rename it to ~/Work/fluxninja/cloud
-if [ -d ~/Work/FluxNinja/RM ]; then
-	mv ~/Work/FluxNinja/RM ~/Work/FluxNinja/cloud
-fi
-
-if [ -d ~/Work/FluxNinja ]; then
-	mv ~/Work/FluxNinja ~/Work/fluxninja
-	# in each repo under ~/Work/fluxninja/* replace "git@github.com:FluxNinja" with "git@github.com:fluxninja" in .git/config
-	for repo in ~/Work/fluxninja/*; do
-		SED="sed"
-		# check whether gsed exists
-		if [ -x "$(command -v gsed)" ]; then
-			SED="gsed"
-		fi
-		$SED -i 's/git@github.com:FluxNinja/git@github.com:fluxninja/g' "$repo"/.git/config
-	done
-fi
-
 gh_clone_all.sh fluxninja ~/Work
 pull_all.sh ~/Work/fluxninja
-
-# check whether ~/Work/fluxninja/cloud/services/aperture is not a symlink
-if ! [ -L ~/Work/fluxninja/cloud/services/aperture ]; then
-	# check whether ~/Work/fluxninja/cloud is on master or main git branch
-	branch="$(git -C ~/Work/fluxninja/cloud rev-parse --abbrev-ref HEAD)"
-	if [ "$branch" == "master" ] || [ "$branch" == "main" ]; then
-		rm -rf ~/Work/fluxninja/cloud/services/aperture
-	fi
-else
-	unlink ~/Work/fluxninja/cloud/services/aperture
-fi
 
 # symlink aperture in cloud
 if [ ! -d ~/Work/fluxninja/cloud/services/aperture ]; then
 	ln -s ~/Work/fluxninja/aperture ~/Work/fluxninja/cloud/services/aperture
-fi
-
-if [ ! -d ~/Work/fluxninja/cloud/services/aperture-go ]; then
-	ln -s ~/Work/fluxninja/aperture-go ~/Work/fluxninja/cloud/services/aperture-go
-fi
-
-if [ ! -d ~/Work/fluxninja/cloud/services/aperture-java ]; then
-	ln -s ~/Work/fluxninja/aperture-java ~/Work/fluxninja/cloud/services/aperture-java
 fi
 
 if [ ! -d ~/Work/fluxninja/aperture-tech-docs/local ]; then
@@ -53,8 +16,6 @@ PRECOMMIT_DIRS=(
 	~/Work/fluxninja/cloud
 	~/Work/fluxninja/aperture
 	~/Work/fluxninja/aperture-tech-docs
-	~/Work/fluxninja/aperture-go
-	~/Work/fluxninja/aperture-java
 )
 
 for dir in "${PRECOMMIT_DIRS[@]}"; do
