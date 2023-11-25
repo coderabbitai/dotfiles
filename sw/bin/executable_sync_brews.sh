c9ece2134bbf63f0b01b8e3fd1adeeb8e477090b
Harjot Gill <harjot.gill@fluxninja.com>
2023-11-24 17:35:19 -0800

    add gh copilot extension

--- sw/bin/executable_sync_brews.sh
+++ sw/bin/executable_sync_brews.sh
@@ -373,7 +373,6 @@ revolver update 'Installing npm packages...'
 # npm stuff
 npm i -q -g bash-language-server
 npm i -q -g dockerfile-language-server-nodejs
-npm i -q -g @githubnext/github-copilot-cli
 
 revolver update 'Installing python packages...'
 #python stuff
