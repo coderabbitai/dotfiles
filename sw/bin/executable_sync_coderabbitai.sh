719fb75c093b36a449cd40bda65a844852aa8cfe
Harjot Gill <harjot.gill@fluxninja.com>
2024-04-20 17:55:03 -0700

    fix sync script

--- sw/bin/executable_sync_coderabbitai.sh
+++ sw/bin/executable_sync_coderabbitai.sh
@@ -7,6 +7,17 @@ pull_all.sh ~/Work/coderabbitai
 # look for package.json files in the subdirectories
 # and run pnpm install in those directories
 find ~/Work/coderabbitai -name package.json -exec dirname {} \; | while read dir; do
-	echo "Installing dependencies in $dir"
-	(cd $dir && pnpm install)
+	# if the directory contains a pnpm-lock.yaml file use pnpm
+	# otherwise look for a yarn.lock file and use yarn
+	# otherwise look for a package-lock.json file and use npm
+	if [ -f "$dir/pnpm-lock.yaml" ]; then
+		echo "Running pnpm install in $dir"
+		(cd $dir && pnpm install)
+	elif [ -f "$dir/yarn.lock" ]; then
+		echo "Running yarn install in $dir"
+		(cd $dir && yarn install)
+	elif [ -f "$dir/package-lock.json" ]; then
+		echo "Running npm install in $dir"
+		(cd $dir && npm install)
+	fi
 done
