b010fa6244930ca82809aca30ccdb9cafd9d9def
Harjot Gill <harjot@coderabbit.ai>
2024-12-11 20:09:48 -0800

    remove rectangle-pro

--- sw/bin/executable_sync_brews.sh
+++ sw/bin/executable_sync_brews.sh
@@ -290,7 +290,6 @@ if [[ $OSTYPE == 'darwin'* ]]; then
 		zoom
     twingate
     chatgpt
-		rectangle-pro
 		swiftdefaultappsprefpane # for setting several macOS properties such as default app for opening plaintext files
     # copyq removed as it likely has memory leak on Apple Silicon
 	)
